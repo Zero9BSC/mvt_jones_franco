0a467b50cd8d347975b217fa7f1b65bafa78af12
Zero9BSC <franconicolasjones@gmail.com>
2022-11-02 15:51:38 -0300

    ediciones

--- mvt_jones_franco/Familiares/views.py
+++ mvt_jones_franco/Familiares/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+#from django.shortcuts import render
+from django.http import HttpResponse
+from models import Padres
 # Create your views here.
+def mostar_familiares(request):
+    madre = Padres(nombre='Marlen', apellido='Jones')
+    saludo = f'Hola a todos esta es mi Familia {madre.nombre}'
+
+    return HttpResponse
+    #return render(request, '', {'nombre': madre, 'apellido': madre})
--- mvt_jones_franco/mvt_jones_franco/urls.py
+++ mvt_jones_franco/mvt_jones_franco/urls.py
@@ -19,5 +19,5 @@ from mvt_jones_franco import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+    re_path('', include('Familiares.url'))
 ]
